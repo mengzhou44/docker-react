09c0fc5fbd42d8517ef4a010cf61229309189210
Meng Zhou <v-menzhou@expediagroup.com>
2021-08-21 08:17:16 -0400

    change page content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Bye there!
-        </p>
+        <p>Elastic Beanstalk</p>
         <a
           className="App-link"
           href="https://reactjs.org"
